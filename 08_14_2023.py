6f07aebe9bd8b92ba8304fd0589d55426c6eef71
nhientattran <nhientattran@gmail.com>
2023-08-14 23:44:32 -0500

    08/14/2023

---
+++ 08_14_2023.py
@@ -0,0 +1,62 @@
+# 88. Merge Sorted Array
+
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+def merge(nums1, m, nums2, n):
+    while m > 0 and n > 0:
+        if nums1[m - 1] >= nums2[n - 1]:
+            nums1[m + n - 1] = nums1[m - 1]
+            m -= 1
+        else:
+            nums1[m + n - 1] = nums2[n - 1]
+            n -= 1
+    while n > 0:
+        nums1[n - 1] = nums2[n - 1]
+        n -= 1
+
+# 27. Remove Element
+
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+def removeElement(nums, val):
+    k = 0
+    i = 0
+    n = len(nums)
+    while i < n:
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+        i += 1
+    return k
+
+# 26. Remove Duplicates from Sorted Array
+
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+# Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+
+def removeDublicates(nums):
+    if not nums:
+        return 0
+    k = 1
+    n = len(nums)
+    for i in range(1, n):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+
+
