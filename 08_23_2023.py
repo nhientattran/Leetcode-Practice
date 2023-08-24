10e50670e0d4269f307be148f98dc32fea6fd030
nhientattran <nhientattran@gmail.com>
2023-08-24 00:05:35 -0500

    08/24/2023

---
+++ 08_23_2023.py
@@ -0,0 +1,48 @@
+# Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+# Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+# Return k after placing the final result in the first k slots of nums.
+
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+    k = 1
+    count = 1
+    for i in range(1,len(nums)):
+        if nums[i] == nums[i-1]:
+            count += 1
+        else:
+            count = 1
+        if count <= 2:
+            nums[k] = nums[i]
+            k += 1
+    return k
+    
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+
+def rotate(nums, k):
+    for count in range(k):
+        prev = nums[-1]
+        for i in range(len(nums)):
+            nums[i], prev = prev, nums[i]
+
+ 
