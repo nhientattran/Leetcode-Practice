24ceabf69699237ce5432b97dc1244763d4ffb1f
nhientattran <nhientattran@gmail.com>
2023-09-08 22:08:06 -0500

    09/08/2023

---
+++ 09_08_2023.py
@@ -0,0 +1,86 @@
+# 167. Two Sum II - Input Array Is Sorted
+
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+# Your solution must use only constant extra space.
+
+# Example 1:
+
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+# Example 2:
+
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+# Example 3:
+
+# Input: numbers = [-1,0], target = -1
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+def twoSum1(numbers, target):
+    out = []
+    for i in range(len(numbers)):
+        sub = target - numbers[i]
+        if sub in numbers[i+1:]:
+            x = numbers[i+1:].index(sub) + i + 1
+            out.append(i+1)
+            out.append(x+1)
+            return out
+
+def twoSum2(numbers, target):
+    left, right = 0, len(numbers) - 1
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+        if current_sum == target:
+            return [left + 1, right + 1]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -=1
+        
+# 11. Container With Most Water
+
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+# Return the maximum amount of water a container can store.
+
+# Notice that you may not slant the container.
+
+# Example 1:
+
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Example 2:
+
+# Input: height = [1,1]
+# Output: 1
+
+def maxArea(height):
+    if len(height) == 2:
+            if height[0] >= height[1]:
+                return height[1] * height[1]
+            else:
+                return height[0] * height[0]
+    area_list = []
+    left, right = 0 , len(height) - 1
+    while left < right:
+        if height[left] >= height[right]:
+            area = height[right] * (right - left)
+            area_list.append(area)
+            right -= 1
+        else:
+            area = height[left] * (right - left)
+            area_list.append(area)
+            left += 1
+    return max(area_list)
+
