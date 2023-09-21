35a7ea3fa0faecf366db58158e2e8161db5e015a
nhientattran <nhientattran@gmail.com>
2023-09-20 21:10:41 -0500

    09/20/2023

---
+++ 09_20_2023.py
@@ -0,0 +1,60 @@
+# 190. Reverse Bits
+
+# Reverse bits of a given 32 bits unsigned integer.
+
+# Note:
+
+# Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+# In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ 
+# Example 1:
+
+# Input: n = 00000010100101000001111010011100
+# Output:    964176192 (00111001011110000010100101000000)
+# Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+# Example 2:
+
+# Input: n = 11111111111111111111111111111101
+# Output:   3221225471 (10111111111111111111111111111111)
+# Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+def reverseBits(self, n):
+    reversed_n = 0
+    for i in range(32):
+        reversed_n <<= 1  
+        reversed_n |= n & 1  
+        n >>= 1  
+    return reversed_n
+
+# 191. Number of 1 Bits
+
+# Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+# Note:
+
+# Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+# In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+ 
+
+# Example 1:
+
+# Input: n = 00000000000000000000000000001011
+# Output: 3
+# Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+# Example 2:
+
+# Input: n = 00000000000000000000000010000000
+# Output: 1
+# Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+# Example 3:
+
+# Input: n = 11111111111111111111111111111101
+# Output: 31
+# Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+def hammingWeight(n):
+    count = 0
+    while n!= 0:
+        count += n & 1
+        n >>= 1
+    return count
