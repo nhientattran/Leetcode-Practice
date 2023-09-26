bbfd18e094d98f49b5acb20eba2c4ca4e18026cc
nhientattran <nhientattran@gmail.com>
2023-09-26 01:46:16 -0500

    09/26/2023

---
+++ 09_25_2023.py
@@ -0,0 +1,73 @@
+# 217. Contains Duplicate
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+
+def containsDuplicate(nums):
+    dic = {}
+    for num in nums:
+        if num not in dic:
+            dic[num] = 1
+        else:
+            dic[num] += 1
+    for value in dic.values():
+        if value > 1:
+            return True
+    return False
+
+
+# 242. Valid Anagram
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+
+def isAnagram(s, t):
+    s_dic = {}
+    t_dic = {}
+    for char in s:
+        if char not in s_dic:
+            s_dic[char] = 1
+        else:
+            s_dic[char] += 1
+    for char in t:
+        if char not in t_dic:
+            t_dic[char] = 1
+        else:
+            t_dic[char] += 1
+    return s_dic == t_dic
+
+print(isAnagram("anagram", "nagaram"))
