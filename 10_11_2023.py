b09080f1eedaf02908ef41a18cc4fbe2ff172894
nhientattran <nhientattran@gmail.com>
2023-10-11 21:40:53 -0500

    10/11/2023

---
+++ 10_11_2023.py
@@ -0,0 +1,104 @@
+# 21. Merge Two Sorted Lists
+
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+ 
+
+# Example 1:
+
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+
+# Input: list1 = [], list2 = []
+# Output: []
+# Example 3:
+
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+ 
+
+# Constraints:
+
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both list1 and list2 are sorted in non-decreasing order.
+
+def mergeTwoLists(list1, list2):
+    dummy = ListNode(-1)
+    current = dummy
+
+    while list1 is not None and list2 is not None:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+    
+    if list1 is not None:
+        current.next = list1
+    
+    if list2 is not None:
+        current.next = list2
+
+    return dummy.next
+
+# 35. Search Insert Position
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums contains distinct values sorted in ascending order.
+# -104 <= target <= 104
+
+def searchInsert(nums, target):
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return i
+        elif nums[i] > target:
+            return i
+        elif i == len(nums) - 1:
+            return i + 1
+        elif nums[i] < target and i + 1 != len(nums) - 1 and nums[i+1] > target:
+            return i + 1
+
+def searchInsert2(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
