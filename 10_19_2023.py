231ca47da8badef3469bf90c1879036615800f65
nhientattran <nhientattran@gmail.com>
2023-10-19 22:01:18 -0500

    10/19/2023

---
+++ 10_19_2023.py
@@ -0,0 +1,73 @@
+# 263. Ugly Number
+
+# An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+# Given an integer n, return true if n is an ugly number.
+
+ 
+
+# Example 1:
+
+# Input: n = 6
+# Output: true
+# Explanation: 6 = 2 × 3
+# Example 2:
+
+# Input: n = 1
+# Output: true
+# Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+# Example 3:
+
+# Input: n = 14
+# Output: false
+# Explanation: 14 is not ugly since it includes the prime factor 7.
+
+def isUgly(n):
+    if n <= 0:
+        return False
+    
+    while n % 2 == 0:
+        n /= 2
+
+    while n % 3 == 0:
+        n /= 3
+
+    while n % 5 == 0:
+        n /= 5
+    
+    return n == 1
+
+# 278. First Bad Version
+
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ 
+
+# Example 1:
+
+# Input: n = 5, bad = 4
+# Output: 4
+# Explanation:
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+# Then 4 is the first bad version.
+# Example 2:
+
+# Input: n = 1, bad = 1
+# Output: 1
+
+def firstBadVersion(n):
+    left, right = 1, n
+    while left < right:
+        mid = left + (right - left) // 2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
