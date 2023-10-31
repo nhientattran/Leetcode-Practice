7b4931f544e349373c21f5eb724cb73804a7f6fd
nhientattran <nhientattran@gmail.com>
2023-10-30 20:15:52 -0500

    10/30/2023

---
+++ 10_30_2023.py
@@ -0,0 +1,77 @@
+import math
+# 367. Valid Perfect Square
+
+# Companies
+# Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+# A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+# You must not use any built-in library function, such as sqrt.
+
+ 
+
+# Example 1:
+
+# Input: num = 16
+# Output: true
+# Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+# Example 2:
+
+# Input: num = 14
+# Output: false
+# Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+ 
+def isPerfectSquare(num):
+    num2 = math.sqrt(num)
+    return num2 == int(num2)
+
+# 374. Guess Number Higher or Lower
+
+# We are playing the Guess Game. The game is as follows:
+
+# I pick a number from 1 to n. You have to guess which number I picked.
+
+# Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+# You call a pre-defined API int guess(int num), which returns three possible results:
+
+# -1: Your guess is higher than the number I picked (i.e. num > pick).
+# 1: Your guess is lower than the number I picked (i.e. num < pick).
+# 0: your guess is equal to the number I picked (i.e. num == pick).
+# Return the number that I picked.
+
+ 
+
+# Example 1:
+
+# Input: n = 10, pick = 6
+# Output: 6
+# Example 2:
+
+# Input: n = 1, pick = 1
+# Output: 1
+# Example 3:
+
+# Input: n = 2, pick = 1
+# Output: 1
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+def guessNumber(n):
+    left, right = 1, n
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        result = guess(mid)
+
+        if result == 0:
+            return mid
+        elif result == -1:
+            right = mid
+        else:
+            left = mid
