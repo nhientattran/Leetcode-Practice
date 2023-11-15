8f4ff4256c7a7dca03644f549f8430f699fe5505
nhientattran <nhientattran@gmail.com>
2023-11-14 20:17:21 -0600

    11/14/2023

---
+++ 11_14_2023.py
@@ -0,0 +1,84 @@
+# 463. Island Perimeter
+
+# You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+# The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+# Example 1:
+
+
+# Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+# Output: 16
+# Explanation: The perimeter is the 16 yellow stripes in the image above.
+# Example 2:
+
+# Input: grid = [[1]]
+# Output: 4
+# Example 3:
+
+# Input: grid = [[1,0]]
+# Output: 4
+
+def islandPerimeter(grid):
+    if not grid:
+        return 0
+    
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+            
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
+
+# 476. Number Complement
+
+# The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+# For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+# Given an integer num, return its complement.
+
+ 
+
+# Example 1:
+
+# Input: num = 5
+# Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+# Example 2:
+
+# Input: num = 1
+# Output: 0
+# Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+def findComplement(num):
+    bin_num = bin(num)[2:]
+    complement = ''
+    out = 0
+    for num in bin_num:
+        if num == '0':
+            complement += '1'
+        elif num == '1':
+            complement += '0'
+    
+    sub = len(complement) - 1
+    sub_2 = 0
+    while sub > -1:
+        out += int(complement[sub_2]) * (2**sub)
+        sub_2 += 1
+        sub -= 1
+    
+    return out
+    
+
